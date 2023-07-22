4f4d253410236dd1749ec51c08cc025866ac3b8c
HadiJF03 <48181301+HadiJF03@users.noreply.github.com>
2023-07-22 04:54:02 +0300

    added rainbow functionality, added reset button js

--- script.js
+++ script.js
@@ -9,20 +9,43 @@ function gridCreator(gridCount, parent){
         parent.append(div);
     }
 }
+let rainbows = false;
+function toggleRainbow(){
+    if(rainbows) rainbows = false;
+    else rainbows= true;
+}
+
 function changeColor(){
-    this.style.background =color
+    if(!rainbows) this.style.background =color;
+    else{
+    const randomColors = "#" + (Math.floor(Math.random()*16777215).toString(16));
+    this.style.background = randomColors;
+    }
+    function randomColor(){
+        l
+        this.style.background = randomColors;
+    }
+}
+
+function resetCanvas(){
+    divs.forEach(div=> div.style.background = "white");
 }
 const canvas = document.querySelector(".canvas");
-console.log(canvas);
-
 gridCreator(16,canvas);
 
 const divs = document.querySelectorAll(".grid")
-console.log(divs)
-divs.forEach(div => div.addEventListener("mouseover",changeColor))
+divs.forEach(div => div.addEventListener("mouseover",changeColor));
+   
 
-let color = "red"
+let color = "red";
 const cButtons = document.querySelectorAll(".color-button");
 cButtons.forEach(cButton => cButton.addEventListener("click", function(){
     color = this.id;
 }))
+
+const rainbowButton = document.querySelector(".rainbow");
+rainbowButton.addEventListener("click",toggleRainbow)
+
+const resetButton = document.querySelector(".reset");
+resetButton.addEventListener("click",resetCanvas);
+
